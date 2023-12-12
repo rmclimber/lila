1851686c6e4ea94a0848c539c62ffdd027134ba3
RM <rmorris.code@gmail.com>
2023-12-12 14:48:33 -0800

    simple data storage object FileFound created

---
+++ Lila/FilesFound.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lila
+{
+    internal class FilesFound
+    {
+        private IEnumerable<string> fullFilenames { get; set; }
+        private IEnumerable<string> truncFilenames { get; set; }
+
+        public FilesFound(string dir, IEnumerable<string> filenames)
+        {
+            fullFilenames = filenames;
+            truncFilenames = TruncateFilenames(dir, filenames);
+        }
+
+        private IEnumerable<string> TruncateFilenames(
+            string dir, 
+            IEnumerable<string> filenames)
+        {
+            var output = new List<string>();
+            foreach (var filename in filenames)
+            {
+                output.Add(filename.Replace(dir, string.Empty));
+            }
+            return output;
+        }
+    }
+}
