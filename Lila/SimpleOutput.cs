351d53aed25766a4bf9f3a90a4a65c2718760a35
Rick Morris <rmorris.code@gmail.com>
2024-01-22 16:24:00 -0800

    installed CSVHelper. this is a very simple, inelegant class to make classification minimally possible.

---
+++ Lila/SimpleOutput.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lila
+{
+    class SimpleOutput
+    {
+        // core instance members
+        // TODO: adjust getters/setters so that mismatch cannot occur
+        private IEnumerable<string> imgNames { get; set; }
+        private IEnumerable<string> imgPaths { get; set; }
+        private IEnumerable<string> labels { get; set; }
+
+        // constructor(s)
+        public SimpleOutput() 
+        { 
+            imgNames = new List<string>();
+            imgPaths = new List<string>();
+            labels = new List<string>();
+        }
+
+        public void AddLabel(string imgName, string imgPath, string label)
+        {
+            imgNames.Append(imgName);
+            imgPaths.Append(imgPath);
+            labels.Append(label);
+        }
+
+        public void ToCsv(string filename)
+        {
+
+        }
+    }
+}
