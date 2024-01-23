622cd83fdeb98f67314822e38251891ac37fdbde
Rick Morris <rmorris.code@gmail.com>
2024-01-23 15:36:38 -0800

    this class added to facilitate usage of CsvHelper

---
+++ Lila/SimpleLabel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lila
+{
+    class SimpleLabel
+    {
+        public int Label { get; set; }
+        public string ImgName { get; set; }
+        public string ImgPath { get; set; }
+    }
+}
