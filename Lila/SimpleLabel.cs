040231f9928e166e28f30ec426ad70bc68067950
Rick Morris <rmorris.code@gmail.com>
2024-01-23 16:05:21 -0800

    recordered code

--- Lila/SimpleLabel.cs
+++ Lila/SimpleLabel.cs
@@ -10,17 +10,21 @@ namespace Lila
 {
     public class SimpleLabel
     {
-        public SimpleLabel(string name, string path, string label) 
-        {
-            ImgName = name;
-            ImgPath = path;
-            if (int.TryParse(label, out int l)) Label = l;
-        }
+        // instance members
         [Name("name")]
         public string ImgName { get; set; }
         [Name("path")]
         public string ImgPath { get; set; }
         [Name("label")]
         public int Label { get; set; }
+        
+
+        // constructor(s)
+        public SimpleLabel(string name, string path, string label) 
+        {
+            ImgName = name;
+            ImgPath = path;
+            if (int.TryParse(label, out int l)) Label = l;
+        }
     }
 }
