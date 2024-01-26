9b108032e67b8810af0df0a194338a55db6d5028
Rick Morris <rmorris.code@gmail.com>
2024-01-25 16:24:42 -0800

    currently not quite passing due to stack overflow during testing

---
+++ LilaTests/SimpleLabelTests.cs
@@ -0,0 +1,39 @@
+using Lila;
+
+namespace LilaTests
+{
+    public class SimpleLabelTests
+    {
+        private string goodNameOrPath = "foo";
+        private string goodPositive = "3";
+        private string goodNegative = "-3";
+        private string badFloat = "3.56";
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void EmptyName()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => new SimpleLabel("", 
+                goodNameOrPath, goodPositive));
+        }
+
+        [Test]
+        public void EmptyPath()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => new SimpleLabel(
+                goodNameOrPath, "", goodPositive));
+        }
+
+        [Test]
+        public void GoodNameAndPath()
+        {
+            SimpleLabel sl = new SimpleLabel(goodNameOrPath, goodNameOrPath, goodPositive);
+            Assert.That(sl.ImgName, Is.EqualTo(goodNameOrPath));
+            Assert.That(sl.ImgPath, Is.EqualTo(goodNameOrPath));
+        }
+    }
+}
